d9b758dcf657987320d6782ae033d23aa84d0ab4
Nazir Ahmad Sz Noori <nazirahmadsaidzada@gmail.com>
2017-11-09 17:47:37 +0800

    Add files via upload

---
+++ Composition_sec_01/Resolution.java
@@ -0,0 +1,19 @@
+package Composition_sec_01;
+
+public class Resolution {
+    private int with;
+    private int height;
+
+    public Resolution(int with, int height) {
+        this.with = with;
+        this.height = height;
+    }
+
+    public int getWith() {
+        return with;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
